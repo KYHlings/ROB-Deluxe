7748b516f43d76afef421544f7aaf4a55ff6b0e7
Samira <samira.joumal@student.kyh.se>
2020-10-08 11:37:30 +0200

    Statistik inlagt

--- ROB - Deluxe/lobby.py
+++ ROB - Deluxe/lobby.py
@@ -3,7 +3,9 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 casino_bg = pygame.image.load('pics//casino.png')
+statistics = pygame.image.load('pics//Statistik.png')
 screen.blit(casino_bg, (0, 0))
+screen.blit(statistics,(800/2, 600/2))
 running = True
 
 while running:
