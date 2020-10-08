7722533049d90090b81c1cb7849ed4c89adda5eb
Samira <samira.joumal@student.kyh.se>
2020-10-08 13:23:04 +0200

    Quitknapp

--- ROB - Deluxe/main_menu.py
+++ ROB - Deluxe/main_menu.py
@@ -7,10 +7,13 @@ logo = pygame.image.load('pics//logga.png')
 screen.blit(logo, (50, 50))
 play_sign = pygame.image.load('pics//play_sign.png')
 screen.blit(play_sign, (250, 250))
-
+quit_sign = pygame.image.load('pics//Quitknapp.png')
+screen.blit(quit_sign, (250, 350))
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     pygame.display.update()
+
+
