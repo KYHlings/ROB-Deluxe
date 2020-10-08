67631a14faa0465fa46ebdfe658aced08ea24acd
Jacob <jacob.nordlund@student.kyh.se>
2020-10-08 11:14:54 +0200

    fix

--- ROB - Deluxe/lobby.py
+++ ROB - Deluxe/lobby.py
@@ -1,3 +1,13 @@
 import pygame
 
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
 casino_bg = pygame.image.load('pics//casino.png')
+screen.blit(casino_bg, (0, 0))
+running = True
+
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+	pygame.display.update()
