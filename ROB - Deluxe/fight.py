45704aee35e0a43820a5564c3f3406f76c7070cc
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-10-12 13:37:20 +0200

    collision

--- ROB - Deluxe/fight.py
+++ ROB - Deluxe/fight.py
@@ -66,24 +66,40 @@ player1.rect.y = 150
 player2 = Player()
 player1_pics(player2)
 player2.rect.x = 200
-player2.rect.y = 50
+player2.rect.y = 150
 
 player_list = pygame.sprite.Group()
 player_list.add(player1, player2)
 
 
-running = True
-
+def collision(player1, player2):
+    col = pygame.sprite.collide_rect(player1, player2)
+    if col == True:
+        print("hit")
+        return True
 
 def player_movement(player1, player2):
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and player1.rect.x > player1.vel:
+        player1.left = True
+        player1.right = False
+        if collision(player1, player2) == True:
+            if player1.left == True:
+                player1.rect.x += 5
+                # det finns en bug där man flyger utanför skärmen om spelarna kolliderar och går åt ett håll tillsammans
+
         player1.rect.x -= 1
         player1.image = pygame.transform.flip(player1.images[player1.frame], True, False)
         player1.frame += 1
         if player1.frame == 2:
             player1.frame = 0
     elif keys[pygame.K_RIGHT] and player1.rect.x < screen_width - 40:
+        player1.left = False
+        player1.right = True
+        if collision(player1, player2) == True:
+            if player1.right == True:
+                player1.rect.x -= 5
+
         player1.rect.x += 1
         player1.frame += 1
         if player1.frame == 2:
@@ -91,12 +107,24 @@ def player_movement(player1, player2):
         player1.image = player1.images[player1.frame]
 
     if keys[pygame.K_a] and player2.rect.x > player2.vel:
+        player2.left = True
+        player2.right = False
+        if collision(player1, player2) == True:
+            if player2.left == True:
+                player2.rect.x += 5
+
         player2.rect.x -= 1
         player2.image = pygame.transform.flip(player2.images[player2.frame], True, False)
         player2.frame += 1
         if player2.frame == 2:
             player2.frame = 0
     elif keys[pygame.K_d] and player2.rect.x < screen_width - 40:
+        player2.left = False
+        player2.right = True
+        if collision(player1, player2) == True:
+            if player2.right == True:
+                player2.rect.x -= 5
+
         player2.rect.x += 1
         player2.frame += 1
         if player2.frame == 2:
@@ -104,6 +132,7 @@ def player_movement(player1, player2):
         player2.image = player2.images[player2.frame]
 
 
+running = True
 while running:
     fps_clock.tick(fps)
     pygame.display.update()
