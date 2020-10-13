9bb6ae1a46785d964f47fadf5b43d651a4db97e2
Jacob <jacob.nordlund@student.kyh.se>
2020-10-13 09:32:29 +0200

    paintpro

--- ROB - Deluxe/lobby.py
+++ ROB - Deluxe/lobby.py
@@ -3,9 +3,11 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 casino_bg = pygame.image.load('pics//casino.png')
-statistics = pygame.image.load('pics//Statistik.png')
+statistics = pygame.image.load('pics//scoreboard.png')
+make_bets = pygame.image.load('pics//make_your_bets.png')
 screen.blit(casino_bg, (0, 0))
-screen.blit(statistics, (800/2, 600/2))
+screen.blit(statistics, (100, 200))
+screen.blit(make_bets, (350, 200))
 running = True
 
 while running:
