e27a30c584b24264bb22d4a550e5f50fa779f7b2
Samira <samira.joumal@student.kyh.se>
2020-10-13 09:47:19 +0200

    fight sign

--- ROB - Deluxe/lobby.py
+++ ROB - Deluxe/lobby.py
@@ -5,9 +5,11 @@ screen = pygame.display.set_mode((800, 600))
 casino_bg = pygame.image.load('pics//casino.png')
 statistics = pygame.image.load('pics//scoreboard.png')
 make_bets = pygame.image.load('pics//make_your_bets.png')
+fight_sign = pygame.image.load('pics//fight_sign.png')
 screen.blit(casino_bg, (0, 0))
 screen.blit(statistics, (100, 200))
 screen.blit(make_bets, (350, 200))
+screen.blit(fight_sign, (250, 50))
 running = True
 
 while running:
