2332dab7415b7f5503dbd2802fbfa2ca20271661
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-10-19 22:53:10 +0200

    la in funktionen för lobby fönstret som vi gjorde tidigare idag, den var borta

--- ROB/lobby.py
+++ ROB/lobby.py
@@ -8,20 +8,7 @@ def lobby():
 	p = Player()
 	pygame.init()
 	pygame.mixer.init()
-	pygame.mixer.music.stop()
-	pygame.mixer.music.load("music//casino_music.wav")
-	pygame.mixer.music.play(-1)
-	screen = pygame.display.set_mode((800, 600))
-	casino_bg = pygame.image.load('pics//casino.png')
-	statistics = pygame.image.load('pics//scoreboard.png')
-	make_bets = pygame.image.load('pics//make_your_bets.png')
-	fight_sign = pygame.image.load('pics//fight_sign.png')
-	fight_button = pygame.Rect(250, 50, 300, 100)
-	pygame.draw.rect(screen, (0, 0, 0),fight_button)
-	screen.blit(casino_bg, (0, 0))
-	screen.blit(statistics, (100, 200))
-	screen.blit(make_bets, (350, 200))
-	screen.blit(fight_sign, (250, 50))
+	fight_button = lobby_window()
 	running = True
 	matchup = 0
 	score_player1 = 0
@@ -47,27 +34,33 @@ def lobby():
 							score_player1 += 100
 						if result == 2:
 							score_player2 += 100
-						print(f"P1: {score_player1}, P2: {score_player2}" )
-						pygame.mixer.music.stop()
-						pygame.mixer.music.load("music//casino_music.wav")
-						pygame.mixer.music.play(-1)
-						screen = pygame.display.set_mode((800, 600))
-						casino_bg = pygame.image.load('pics//casino.png')
-						statistics = pygame.image.load('pics//scoreboard.png')
-						make_bets = pygame.image.load('pics//make_your_bets.png')
-						fight_sign = pygame.image.load('pics//fight_sign.png')
-						fight_button = pygame.Rect(250, 50, 300, 100)
-						pygame.draw.rect(screen, (0, 0, 0), fight_button)
-						screen.blit(casino_bg, (0, 0))
-						screen.blit(statistics, (100, 200))
-						screen.blit(make_bets, (350, 200))
-						screen.blit(fight_sign, (250, 50))
+						print(f"P1: {score_player1}, P2: {score_player2}")
+						lobby_window()
+
 						# gå vidare till nästa match
 						matchup += matchup
 
 
 			# uppdaterar displayen
 			pygame.display.update()
+
+
+def lobby_window():
+	pygame.mixer.music.stop()
+	pygame.mixer.music.load("music//casino_music.wav")
+	pygame.mixer.music.play(-1)
+	screen = pygame.display.set_mode((800, 600))
+	casino_bg = pygame.image.load('pics//casino.png')
+	statistics = pygame.image.load('pics//scoreboard.png')
+	make_bets = pygame.image.load('pics//make_your_bets.png')
+	fight_sign = pygame.image.load('pics//fight_sign.png')
+	fight_button = pygame.Rect(250, 50, 300, 100)
+	pygame.draw.rect(screen, (0, 0, 0), fight_button)
+	screen.blit(casino_bg, (0, 0))
+	screen.blit(statistics, (100, 200))
+	screen.blit(make_bets, (350, 200))
+	screen.blit(fight_sign, (250, 50))
+	return fight_button
 
 
 
