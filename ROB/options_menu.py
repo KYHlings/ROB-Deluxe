031f55c98b6f6f054036ddf5f1312b95fc9d0c24
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-10-27 09:02:26 +0100

    bakgrund om man blir träffad

---
+++ ROB/options_menu.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode(600,800)
+font = pygame.font.SysFont("Arial", 40, True)
+
+def options():
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return
+        screen.blit(font.render(f"Press [SPACE] to celebrate", True, (black)), (screen_width/3, 90))
